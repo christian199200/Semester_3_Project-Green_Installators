7aa72ac8fbc94ffbd257fc3a6ef9763399955769
Christian Tchoumkeu Nya <151011464+christian199200@users.noreply.github.com>
2025-03-04 04:16:42 +0100

    Update CompanyInfo.java
    init

--- src/main/java/com/via/greeninstalators/model/CompanyInfo.java
+++ src/main/java/com/via/greeninstalators/model/CompanyInfo.java
@@ -17,13 +17,15 @@ public class CompanyInfo implements Serializable {
     private String country;
     private String postal_code;
     private double price_per_distance_unit;
+    private String email
 
     //default constructor for JPA
     public CompanyInfo() {}
 
-    public CompanyInfo(String companyCode, String name, String country, String postal_code, double price_per_distance_unit) {
+    public CompanyInfo(String companyCode, Sting email, String name, String country, String postal_code, double price_per_distance_unit) {
         this.companyCode = companyCode;
         this.name = name;
+        this.email = email
         this.country = country;
         this.postal_code = postal_code;
         this.price_per_distance_unit = price_per_distance_unit;
@@ -40,6 +42,10 @@ public class CompanyInfo implements Serializable {
 
     public String getName() {
         return name;
+    }
+
+     public String email() {
+        return email;
     }
 
     public String getCountry() {
